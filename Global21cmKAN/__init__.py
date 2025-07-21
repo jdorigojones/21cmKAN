92e0e80f478ea8bf00237464d1db28669b9bd1c9
Johnny Dorigo Jones <89427403+jdorigojones@users.noreply.github.com>
2025-07-21 16:36:53 -0600

    Add files via upload

---
+++ Global21cmKAN/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+__version__ = "1.0"
+__author__ = "Johnny Dorigo Jones"
+
+import requests
+import os
+from pathlib import Path
+#from Global21cmKAN import __path__
+
+from Global21cmKAN import emulator_21cmKAN_21cmGEM
+from Global21cmKAN import emulator_21cmKAN_ARES
