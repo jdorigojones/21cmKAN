1a18534d78e0ab772e9b683dfd759420b920bfc7
Johnny Dorigo Jones <89427403+jdorigojones@users.noreply.github.com>
2025-08-07 00:52:32 -0600

    Update KAN_pymultinest_job.sh

--- scripts/KAN_pymultinest_job.sh
+++ scripts/KAN_pymultinest_job.sh
@@ -6,8 +6,6 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --job-name=21cmKAN_pymultinest_1
 #SBATCH --output=21cmKAN_pymultinest_1.%j.out
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=jodo2960@colorado.edu
 
 ml purge
 ml miniforge
@@ -16,12 +14,9 @@ mamba activate 21cm-kan-env
 export PATH=$PATH:$HOME/.local/bin/
 export UCX_TLS=ud,sm,self
 export SLURM_EXPORT_ENV=ALL
-export LD_LIBRARY_PATH=/projects/jodo2960/KAN/MultiNest/lib:$LD_LIBRARY_PATH
-export PERSES=/projects/jodo2960/KAN/perses
-export PYLINEX=/projects/jodo2960/KAN/pylinex
-export DISTPY=/projects/jodo2960/KAN/distpy
-export ARES=/projects/jodo2960/KAN/ares
+export LD_LIBRARY_PATH=/path/to/MultiNest/lib:$LD_LIBRARY_PATH
+export PYLINEX=/path/to/pylinex
+export DISTPY=/path/to/distpy
 
 cd 21cmKAN/
-
 python pymultinest_21cmKAN_21cmGEM.py
