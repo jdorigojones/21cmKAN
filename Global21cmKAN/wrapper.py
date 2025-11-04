7ea4d44d21be175e54b3e3b4e27f24f8c3bbaa2a
Johnny Dorigo Jones <89427403+jdorigojones@users.noreply.github.com>
2025-11-04 14:35:16 -0700

    Update wrapper.py

--- Global21cmKAN/wrapper.py
+++ Global21cmKAN/wrapper.py
@@ -10,7 +10,7 @@ import os
 import numpy as np
 from pylinex import LoadableModel
 import Global21cmKAN as Global21cmKAN
-from Global21cmKAN.evaluate import evaluate_on_21cmGEM, evaluate_on_ARES
+from Global21cmKAN.evaluate import evaluate_on_21cmGEM, evaluate_on_ARES, evaluate_on_foreground
 
 try:
 	# this runs with no issues in python 2 but raises error in python 3
@@ -22,6 +22,7 @@ except:
 PATH = f"{os.environ.get('AUX_DIR', os.environ.get('HOME'))}/.Global21cmKAN/"
 model_save_path_21cmGEM = PATH+"models/emulator_21cmGEM.pth"
 model_save_path_ARES = PATH+"models/emulator_ARES.pth"
+model_save_path_foreground = PATH+"models/emulator_foreground_beam.pth"
 
 class predict_21cmGEM(LoadableModel):
 	def __init__(self, parameters, model_path=model_save_path_21cmGEM):
@@ -95,6 +96,47 @@ class predict_ARES(LoadableModel):
 	def neural_network_predictor(self):
 		if not hasattr(self, '_neural_network_predictor'):
 			self._neural_network_predictor = evaluate_on_ARES(model_path=self.model_path)
+		return self._neural_network_predictor
+	
+	def __call__(self, parameters):
+		'''
+  		parameters: np.ndarray
+    			list of parameters to accept as input
+       		'''
+		signal = self.neural_network_predictor(parameters)
+		return signal
+
+class predict_foreground(LoadableModel):
+	def __init__(self, parameters, model_path=model_save_path_foreground):
+		'''
+  		parameters: np.ndarray
+    			list of parameters to accept as input
+       		model_path : str
+	 		The path to the saved 21cmKAN model instance
+    			Default: f"{os.environ.get('AUX_DIR', os.environ.get('HOME'))}/.Global21cmKAN/"+"models/emulator_foreground_beam.pth"
+       		'''
+		self.parameters = parameters
+		self.model_path = model_path
+	
+	@property
+	def parameters(self):
+		"""
+  		Property storing an array of parameters for this model
+    		"""
+		return self._parameters
+	
+	@parameters.setter
+	def parameters(self, value):
+		"""
+  		Setter for the array of parameters for this model
+    		value: array of parameters to give to the evaluate_on_foreground.__call__() function
+      		"""
+		self._parameters = [element for element in value]
+	
+	@property
+	def neural_network_predictor(self):
+		if not hasattr(self, '_neural_network_predictor'):
+			self._neural_network_predictor = evaluate_on_foreground(model_path=self.model_path)
 		return self._neural_network_predictor
 	
 	def __call__(self, parameters):
